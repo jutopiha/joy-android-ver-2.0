2d370ac75a7bd5e0b7dbf073bca12b4a5f7c4975
조현정 <guswjd8718@naver.com>
2017-11-12 21:17:02 +0900

    개인정보 수정 기본 setting

---
+++ app/src/main/java/com/joy/tiggle/joy/Activity/ModifyInfoActivity.java
@@ -0,0 +1,18 @@
+package com.joy.tiggle.joy.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.joy.tiggle.joy.R;
+
+/**
+ * Created by CE-L-17 on 2017-11-12.
+ */
+
+public class ModifyInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_info);
+    }
+}
--- app/src/main/java/com/joy/tiggle/joy/Activity/SettingActivity.java
+++ app/src/main/java/com/joy/tiggle/joy/Activity/SettingActivity.java
@@ -94,6 +94,11 @@ public class SettingActivity extends AppCompatActivity implements View.OnClickLi
                 startActivity(intent);
                 finish();
                 break;
+            case R.id.btnModifyInfo:
+                intent = new Intent(getApplicationContext(), ModifyInfoActivity.class);
+                startActivity(intent);
+                finish();
+                break;
             case R.id.btnPCMode:
                 intent = new Intent(getApplicationContext(), PCGuideActivity.class);
                 startActivity(intent);
